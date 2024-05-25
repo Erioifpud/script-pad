8a7b8c9373ce0cfa6e42af5a1f56ea021115c530
Erioifpud <erioifpud@zuo.si>
2024-05-25 18:25:57 +0800

    feat: 新增编辑页设置按钮

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@/components/ui/button';
+import { GearIcon } from '@radix-ui/react-icons';
+
 // 获取数据，预生成指定静态页面的，用作 SEO
 export async function generateStaticParams() {
   return [
@@ -9,7 +12,17 @@ export default function Edit() {
   return (
     <div className="w-full h-full flex flex-col">
       <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
-        <h1 className="text-xl font-semibold">Playground</h1>
+        <h1 className="text-xl font-semibold">
+          草稿 1
+        </h1>
+        <Button
+          variant="outline"
+          size="sm"
+          className="ml-auto gap-1.5 text-sm"
+        >
+          <GearIcon className="size-3.5" />
+          设置
+        </Button>
       </header>
     </div>
   );
