840274c46a3d8d8e0e83e914808563029704d4ae
Erioifpud <erioifpud@zuo.si>
2024-06-01 00:27:32 +0800

    feat: 新增首页跳转

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    redirect("/edit")
+  }, [])
   return (
     <div>
       <Link href="/edit">Edit</Link>
