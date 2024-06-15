ed42da995b9ce5d9a87e83124b878268ecbe6a97
Erioifpud <erioifpud@zuo.si>
2024-06-15 21:34:42 +0800

    feat: 新增打开目录 command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use showfile;
+
 fn main() {
   tauri::Builder::default()
     .plugin(tauri_plugin_store::Builder::default().build())
     .plugin(tauri_plugin_clipboard::init())
     .invoke_handler(tauri::generate_handler![open_devtools])
+    .invoke_handler(tauri::generate_handler![open_directory])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
@@ -13,4 +16,9 @@ fn main() {
 #[tauri::command]
 fn open_devtools(window: tauri::Window) {
   window.open_devtools();
+}
+
+#[tauri::command]
+fn open_directory(path: String) {
+  showfile::show_path_in_file_manager(path);
 }
