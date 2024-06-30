9fb72254f23855cb3b92b0c0d787b56063bc504d
Erioifpud <erioifpud@zuo.si>
2024-06-30 18:54:01 +0800

    feat: 新增 useCurrentGroup hook

---
+++ src/pages/LowCode/hooks/useCurrentGroup.ts
@@ -0,0 +1,11 @@
+import { useLowCodeStore } from '@/store/lowcode'
+import { useCommonStore } from '@/store/common'
+import { useMemo } from 'react'
+
+export const useCurrentGroup = () => {
+  const selectedGroupId = useCommonStore((state) => state.selectedGroupId)
+  const groups = useLowCodeStore((state) => state.groups)
+  return useMemo(() => {
+    return groups.find((group) => group.id === selectedGroupId)
+  }, [selectedGroupId, groups])
+}
