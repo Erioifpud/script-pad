22df07d248a4f91d611db797a02d932900b00cbb
Erioifpud <erioifpud@zuo.si>
2024-07-01 14:19:35 +0800

    feat: 暂时移除层级修改功能

--- src/pages/LowCode/components/ToolPanel/NodeTools/BasicInfo.tsx
+++ src/pages/LowCode/components/ToolPanel/NodeTools/BasicInfo.tsx
@@ -1,9 +1,8 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { LowCodeContext } from '@/pages/LowCode/context/LowCodeContext/context';
-import useOtherNodes from '@/pages/LowCode/hooks/useOtherNodes';
 import { useLowCodeStore } from '@/store/lowcode';
 import { produce } from 'immer';
-import { memo, useCallback, useContext, useMemo } from 'react';
+import { memo, useCallback, useContext } from 'react';
 
 // 常见的就行
 const tagNameOptions: { label: string, value: keyof HTMLElementTagNameMap }[] = [
@@ -17,22 +16,6 @@ const tagNameOptions: { label: string, value: keyof HTMLElementTagNameMap }[] = 
 const BasicInfo = memo(() => {
   const { currentNode, currentGroup } = useContext(LowCodeContext);
   const setGroup = useLowCodeStore(state => state.setGroup);
-
-  const otherNodes = useOtherNodes(currentGroup, currentNode?.id || '');
-
-  const otherNodesOptions = useMemo(() => {
-    return otherNodes.map(node => {
-      return {
-        label: (
-          <div className="flex items-center">
-            <div className="mr-2">{node.type}</div>
-            <div className="text-gray-400 ml-auto text-xs">[{node.id}]</div>
-          </div>
-        ),
-        value: node.id
-      }
-    })
-  }, [otherNodes])
 
   const handleTypeChange = useCallback((value: keyof HTMLElementTagNameMap) => {
     if (!currentGroup || !currentNode) return;
@@ -58,22 +41,6 @@ const BasicInfo = memo(() => {
               </SelectTrigger>
               <SelectContent>
                 {tagNameOptions.map(option => {
-                  return <SelectItem key={option.value} value={option.value}>{option.label}</SelectItem>
-                })}
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-
-        <div className="item mb-4">
-          <div className="label mb-2 text-sm">父节点</div>
-          <div className="value bg-white">
-            <Select>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder={currentNode?.type} />
-              </SelectTrigger>
-              <SelectContent>
-                {otherNodesOptions.map(option => {
                   return <SelectItem key={option.value} value={option.value}>{option.label}</SelectItem>
                 })}
               </SelectContent>
