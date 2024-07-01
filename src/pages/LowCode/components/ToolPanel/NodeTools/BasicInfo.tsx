2df93909154c30f3208cccae96d7fd5386f4c791
Erioifpud <erioifpud@zuo.si>
2024-07-01 21:27:39 +0800

    feat: 新增节点内容修改

--- src/pages/LowCode/components/ToolPanel/NodeTools/BasicInfo.tsx
+++ src/pages/LowCode/components/ToolPanel/NodeTools/BasicInfo.tsx
@@ -1,8 +1,9 @@
+import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { LowCodeContext } from '@/pages/LowCode/context/LowCodeContext/context';
 import { useLowCodeStore } from '@/store/lowcode';
 import { produce } from 'immer';
-import { memo, useCallback, useContext } from 'react';
+import { ChangeEvent, memo, useCallback, useContext } from 'react';
 
 // 常见的就行
 const tagNameOptions: { label: string, value: keyof HTMLElementTagNameMap }[] = [
@@ -17,6 +18,7 @@ const BasicInfo = memo(() => {
   const { currentNode, currentGroup } = useContext(LowCodeContext);
   const setGroup = useLowCodeStore(state => state.setGroup);
 
+  // 修改节点类型
   const handleTypeChange = useCallback((value: keyof HTMLElementTagNameMap) => {
     if (!currentGroup || !currentNode) return;
     const newGroup = produce(currentGroup, draft => {
@@ -24,6 +26,19 @@ const BasicInfo = memo(() => {
       const node = nodes.find(node => node.id === currentNode?.id)
       if (node) {
         node.type = value
+      }
+    })
+    setGroup(newGroup.id, newGroup)
+  }, [currentGroup, currentNode, setGroup])
+
+  // 修改节点内容
+  const handleValueChange = useCallback((ev: ChangeEvent<HTMLInputElement>) => {
+    if (!currentGroup || !currentNode) return;
+    const newGroup = produce(currentGroup, draft => {
+      const nodes = draft.nodes
+      const node = nodes.find(node => node.id === currentNode?.id)
+      if (node) {
+        node.value = ev.target.value
       }
     })
     setGroup(newGroup.id, newGroup)
@@ -45,6 +60,18 @@ const BasicInfo = memo(() => {
                 })}
               </SelectContent>
             </Select>
+          </div>
+        </div>
+
+        <div className="item mb-4">
+          <div className="label mb-2 text-sm">节点类型</div>
+          <div className="value bg-white">
+            <Input
+              type="text"
+              placeholder="元素的内容"
+              value={currentNode?.value || ''}
+              onChange={handleValueChange}
+            />
           </div>
         </div>
       </div>
