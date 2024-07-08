f882bd4c671eca8596535b0250263bbd51ee9fde
Erioifpud <erioifpud@zuo.si>
2024-07-08 20:25:59 +0800

    feat: 实现 http 服务启动

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod server;
+
 use showfile;
+use std::thread;
+use server::init;
 
 fn main() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_store::Builder::default().build())
-    .plugin(tauri_plugin_clipboard::init())
-    .invoke_handler(tauri::generate_handler![open_devtools, open_directory])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+            let boxed_handle = Box::new(handle);
+
+            thread::spawn(move || {
+                init(*boxed_handle).unwrap()
+            });
+            Ok(())
+        })
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_clipboard::init())
+        .invoke_handler(tauri::generate_handler![open_devtools, open_directory])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
 fn open_devtools(window: tauri::Window) {
-  window.open_devtools();
+    window.open_devtools();
 }
 
 #[tauri::command]
 fn open_directory(path: String) {
-  showfile::show_path_in_file_manager(path);
+    showfile::show_path_in_file_manager(path);
 }
---
+++ src-tauri/src/server/mod.rs
@@ -0,0 +1,26 @@
+mod handlers;
+
+use std::sync::Mutex;
+use actix_web::{HttpServer, App, web, middleware};
+use tauri::AppHandle;
+
+struct TauriAppState {
+    app: Mutex<AppHandle>,
+}
+
+#[actix_web::main]
+pub async fn init(app: AppHandle) -> std::io::Result<()> {
+    let tauri_app = web::Data::new(TauriAppState {
+        app: Mutex::new(app)
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(tauri_app.clone())
+            .wrap(middleware::Logger::default())
+            .service(handlers::example::handle)
+    })
+        .bind(("127.0.0.1", 56789))?
+        .run()
+        .await
+}
