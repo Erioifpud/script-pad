2ed80896c5e9851b29561f3c925c8f4e6e4934dc
Erioifpud <erioifpud@zuo.si>
2024-07-10 12:11:23 +0800

    feat: 尝试解析 json body

--- src-tauri/src/server/handlers/example.rs
+++ src-tauri/src/server/handlers/example.rs
@@ -1,12 +1,27 @@
 use actix_web::{post};
-use actix_web::web::Data;
+use actix_web::web::{Data, Json};
 use tauri::Manager;
+use bytes::BytesMut;
+use serde::{Deserialize, Serialize};
 use crate::server::TauriAppState;
 
-#[post("/api/test")]
-pub async fn handle(data: Data<TauriAppState>) -> actix_web::Result<String> {
+#[derive(Deserialize, Serialize)]
+struct RunInfo {
+    id: String,
+}
+
+impl Clone for RunInfo {
+    fn clone(&self) -> Self {
+        RunInfo {
+            id: self.id.clone(),
+        }
+    }
+}
+
+#[post("/api/run")]
+pub async fn handle(data: Data<TauriAppState>, info: Json<RunInfo>) -> actix_web::Result<String> {
     let app = data.app.lock().unwrap();
-    app.emit_all("test", "test").unwrap();
+    app.emit_all("test", info.into_inner()).unwrap();
 
     let text = "hello world";
     println!("{}",text);
