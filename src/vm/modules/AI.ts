a51f9dd653324299a7eb2c9c96b2d41fab53e081
Erioifpud <erioifpud@zuo.si>
2024-08-02 02:11:53 +0800

    fix: 修复调用语法错误

--- src/vm/modules/AI.ts
+++ src/vm/modules/AI.ts
@@ -62,7 +62,7 @@ interface QwenChatOptions {
 
 export class AI {
   async qwenChatRaw(options: QwenChatRawOptions) {
-    const resp = await Request.post('https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation', {
+    const resp = await new Request().post('https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation', {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${options.key}`
