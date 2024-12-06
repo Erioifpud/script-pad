f76d9f94514d80c5784b0bb162b028d0871fe838
Erioifpud <erioifpud.cn@gmail.com>
2024-12-06 22:25:32 +0800

    fix: 调整端口号，避免与其他 tauri 项目冲突

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   clearScreen: false,
   // Tauri expects a fixed port, fail if that port is not available
   server: {
-    strictPort: true
+    strictPort: true,
+    port: 51630,
   },
   // to access the Tauri environment variables set by the CLI with information about the current target
   envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
